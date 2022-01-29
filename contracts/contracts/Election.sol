0d0032d88566b771834c96a025d59e8daf621814
Viktor Meduneckij <viktor.meduneckij@tesonet.com>
2022-01-29 16:26:47 +0200

    initialize repo

---
+++ contracts/contracts/Election.sol
@@ -0,0 +1,31 @@
+pragma solidity >=0.4.16 <0.9.0;
+
+contract Election {
+    struct Candidate {
+        uint256 id;
+        string name;
+        uint256 voteCount;
+    }
+
+    mapping(uint256 => Candidate) public candidates;
+    mapping(address => bool) public voters;
+
+    uint256 public candidatesCount;
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        require(!voters[msg.sender]);
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        voters[msg.sender] = true;
+        candidates[_candidateId].voteCount++;
+    }
+
+    constructor() {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+}
